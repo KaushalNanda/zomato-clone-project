9faa571f516a263f092acd88ef71d081ca0b0190
Kaushal_Nanda <kaushalnanda2610@gmail.com>
2022-05-09 10:07:34 +0530

    js-demo

---
+++ script.js
@@ -0,0 +1,11 @@
+// console.log("Kaushal")
+// for (let number = 0; number <= 10; number += 1) {
+//   console.log(number++)
+// }
+// const arr = [1,2,3];
+// console.log(typeof "k");
+const arr={
+    name:"kaushal",
+    brand: "iphone 6s"
+}
+console.log(arr)
